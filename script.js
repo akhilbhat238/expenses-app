0d21212473d70b173160904a5c1bbb155431ff86
akhilbhat238 <30746769+akhilbhat238@users.noreply.github.com>
2023-01-12 22:39:31 +0530

    Selecting and Switching Tabs

--- script.js
+++ script.js
@@ -5,6 +5,64 @@ const detailsDiv = document.querySelector(".details-viz-div");
 const detailsTabs = document.querySelector(".details-viz-tabs");
 const detailsTable = document.querySelector(".details-viz-table");
 const dispTable = document.querySelector("#finance-table");
+const dynamicTable = document.querySelector(".table-container");
+const dynamicTableCell = document.querySelector(".table-container-cell");
+
+const btnTab1 = document.querySelector("#tab1");
+const btnTab2 = document.querySelector("#tab2");
+
+let activeTab;
+
+const getActiveTab = function () {
+  return document.querySelector(".active");
+};
+
+var tabDict = {
+  tab1: "expenses_by_length",
+  tab2: "expenses_by_category",
+};
+
+const addGrids = function (gridCount) {
+  let columnCount;
+  columnCount = gridCount == 2 ? "double" : "triple";
+  columnCount = columnCount + "-col";
+  console.log(columnCount);
+  dynamicTable.classList.remove("single-col");
+  dynamicTable.classList.add(columnCount);
+  for (let grid = 1; grid <= gridCount; grid++) {
+    dynamicTable.appendChild(dynamicTableCell);
+  }
+};
+
+const getFinanceData = function (tabId) {
+  console.log(tabId);
+  const tabLink = `https://shark-app-pytes.ondigitalocean.app/${tabDict[tabId]}`;
+  const request = new XMLHttpRequest();
+  request.open("GET", tabLink);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+    const numEntries = Object.keys(data).length;
+    const gridCount = Math.ceil(numEntries / 5);
+    if (gridCount == 2) {
+      addGrids(2);
+    } else if (gridCount >= 3) {
+      addGrids(3);
+    }
+
+    // for (const property in data) {
+    //   console.log(`${property}: ${data[property]}`);
+    //   var row = dispTable.insertRow(1);
+    //   var cell1 = row.insertCell(0);
+    //   var cell2 = row.insertCell(1);
+    //   cell1.innerHTML = property;
+    //   cell2.innerHTML =
+    //     Math.round((data[property] + Number.EPSILON) * 100) / 100;
+    // }
+  });
+};
 
 const flipFunction = function (inputElement) {
   if (inputElement.classList.contains("down")) {
@@ -13,39 +71,29 @@ const flipFunction = function (inputElement) {
     detailsDiv.classList.remove("hidden");
     detailsTabs.classList.remove("hidden");
     detailsTable.classList.remove("hidden");
+    activeTab = getActiveTab();
+    console.log(activeTab.id);
+    getFinanceData(activeTab.id);
   } else {
     arrowButton.classList.remove("up");
     arrowButton.classList.add("down");
-    detailsDiv.classList.remove("hidden");
+    detailsDiv.classList.add("hidden");
     detailsTabs.classList.add("hidden");
     detailsTable.classList.add("hidden");
   }
-};
-
-const getFinanceData = function () {
-  const request = new XMLHttpRequest();
-  request.open(
-    "GET",
-    "https://shark-app-pytes.ondigitalocean.app/expenses_by_category"
-  );
-  request.send();
-
-  request.addEventListener("load", function () {
-    const data = JSON.parse(this.responseText);
-    for (const property in data) {
-      console.log(`${property}: ${data[property]}`);
-      var row = dispTable.insertRow(1);
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      cell1.innerHTML = property;
-      cell2.innerHTML =
-        Math.round((data[property] + Number.EPSILON) * 100) / 100;
-    }
-  });
 };
 
 // getFinanceData();
 
 arrowButton.addEventListener("click", function () {
   flipFunction(arrowButton);
+  // addGrids(3);
+});
+
+btnTab2.addEventListener("click", function () {
+  btnTab1.classList.remove("active");
+  btnTab2.classList.add("active");
+  activeTab = getActiveTab();
+  console.log(activeTab.id);
+  getFinanceData(activeTab.id);
 });
